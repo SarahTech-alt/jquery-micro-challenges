d3905f22148cbe6fbad7aaaf2b31a085566d0226
Sarah Fuoss <sfuoss@gmail.com>
2021-08-27 12:00:33 -0500

    completed fourth challenge

--- challenges/130-appendOnClick.js
+++ challenges/130-appendOnClick.js
@@ -11,7 +11,10 @@ function appendBulletItem(){
   $('.container').append(header);
   let button = $('<button id="appendButton">Click Me</button>');
   $('.container').append(button);
-
+  let clickMessage = $('<p>click</p>');
+  $(button).on('click', function(){
+    $('.container').append(clickMessage);
+  });
   // YOUR CODE HERE
   // Append a new p tag with text "click" to the DOM on a click
 }
